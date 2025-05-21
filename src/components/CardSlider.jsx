7c45050cb430bf5ae371eeff47edc721ed632485
devvvvvs <dicasdeacademia@gmail.com>
2025-05-21 12:24:16 -0300

    commit

--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -25,7 +25,7 @@ const CardSlider = ( { produtos } ) => {
       <ArrowBackIosIcon className="nav-button left" onClick={() => scroll('left')}></ArrowBackIosIcon>
       <div className="cards-wrapper" ref={containerRef}>
         {produtos.map((produto, index) => (
-          <Box key={index} sx={{ minWidth: {xs:'345px', sm:'345px', lg:'345px'}, minHeight: {xs:'462px', sm:'462px', lg:'462px'}}}>
+          <Box key={index} sx={{ minWidth: {xs:'325px', sm:'325px', lg:'325px'}}}>
             <CardProduto produto={produto} />
           </Box>
         ))}
